8845f123d27ea459666212c9fe5093ea1f1c0e3e
Sebastian <sebastian.danielsson@protonmail.com>
2020-09-25 00:55:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # move server files into shared folder that the host can access
 if [ -d "/etlegacy-v2.76-x86_64" ];
@@ -8,6 +8,4 @@ if [ -d "/etlegacy-v2.76-x86_64" ];
 fi
 
 # launch server
-ls -l /
-ls -l /etlegacy
-exec ./etlded_bot.sh
+./etlded +set g_protect 1 +set omnibot_enable 1 +set omnibot_path "./legacy/omni-bot" +exec etl_server.cfg 
