6366e9cfe802dba245a32261550a87a6e2f570ee
Sebastian <sebastian.danielsson@protonmail.com>
2020-09-25 00:00:51 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-ls -l
-ls -l /etlegacy-v2.76-x86_64
-ls -l /etlegacy
-
 # move server files into shared folder that the host can access
-if [ -d "etlegacy-v2.76-x86_64" ]; then
-  mv  -v /etlegacy-v2.76-x86_64/* /etlegacy/
-  echo "Renaming mv etlegacy-v2.76-x86_64 etlegacy"
+if [ -d "etlegacy-v2.76-x86_64" ];
+  then
+    mv -v /etlegacy-v2.76-x86_64/* /etlegacy/
+    rm -R /etlegacy-v2.76-x86_64
 fi
 
 # launch server
-
-ls -l
-ls -l /etlegacy-v2.76-x86_64
-ls -l /etlegacy
-
-chmod 700 ./etlded_bot.sh
-echo "Trying to exec etlded_bot.sh, let's see where it breaks"
-
-ls -l
-ls -l /etlegacy-v2.76-x86_64
-ls -l /etlegacy
-
-etlded_bot.sh
+cd /etlegacy
+./etlded_bot.sh
