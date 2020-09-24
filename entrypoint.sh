45d8eefbebdca5b9f1914c64f5883f143c108654
Sebastian <sebastian.danielsson@protonmail.com>
2020-09-25 00:45:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # move server files into shared folder that the host can access
-if [ -d "etlegacy-v2.76-x86_64" ];
+if [ -d "/etlegacy-v2.76-x86_64" ];
   then
     mv -v /etlegacy-v2.76-x86_64/* /etlegacy/
     rm -R /etlegacy-v2.76-x86_64
